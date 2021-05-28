b19e24b94820bcc5f2e070f2e139cb18109157b1
Klrushka <76685224+Klrushka@users.noreply.github.com>
2021-05-28 10:04:53 +0300

    Add files via upload

---
+++ ControllingExecution/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+    public static void main(String[] args) {
+
+        /* Task4
+        tested in between [2..1000000]
+        now between [2..100]
+         */
+        Task4.simpleNumbers();
+
+        // Task 5
+
+        System.out.println("\nTask 5: 9 = " + Task5.intToBinaryString(9));
+
+        // Task 9
+        System.out.print("Task 9: ");
+        Task9.fib(10);
+        System.out.println('\n');
+
+        // Task 10
+        System.out.println("Task 10 Vampire numbers: ");
+        Task10.vampireNumbers();
+    }
+}
